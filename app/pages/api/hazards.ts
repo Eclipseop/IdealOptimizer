1fdedc4af9a5f5cdfd213e9d32741d01d9baa612
eclipseop <guest173800@gmail.com>
2021-03-30 19:16:09 -0400

    more hazards added

--- app/pages/api/hazards.ts
+++ app/pages/api/hazards.ts
@@ -10,7 +10,7 @@ interface FloorData {
 // Forgive me father for I have sinned by writing this code
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	const data = (
-		await axios.get("http://puzzledragonx.com/en/mission.asp?m=4552")
+		await axios.get("http://puzzledragonx.com/en/mission.asp?m=4720")
 	).data;
 	const $ = cheerio.load(data);
 	$("#tabledrop tr").each((i, item) => {
@@ -28,7 +28,31 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 		const html = skills.html();
 		const hazards = [];
 		if (html.includes("Resolve")) {
-			hazards.push("Resolve");
+			if (html.includes("Super")) {
+				hazards.push("SuperResolve");
+			} else {
+				hazards.push("Resolve");
+			}
+		} else if (html.includes("Disable active skills")) {
+			hazards.push("SkillBind");
+		} else if (html.includes("Increases skyfall chance of Jammer orbs")) {
+			hazards.push("Skyfall_Jammer");
+		} else if (html.includes("Disable awoken skills")) {
+			hazards.push("AwokenBind");
+		} else if (html.includes("spawn Poison orbs")) {
+			hazards.push("PoisonSpawn");
+		} else if (html.includes("into bomb")) {
+			hazards.push("BombSpawn");
+		} else if (html.includes("Randomly hide")) {
+			hazards.push("Blind");
+		} else if (html.includes("Absorbs single hit damage over")) {
+			hazards.push("DmgAbsorb");
+		} else if (html.includes(" damage you cause for combos")) {
+			hazards.push("ComboShield");
+			//TODO: find the actual combo number
+		} else if (html.includes("are unmatchable")) {
+			hazards.push("Unmatchable");
+			//TODO: Find actual color
 		}
 
 		const BIG_DATA: FloorData = {
