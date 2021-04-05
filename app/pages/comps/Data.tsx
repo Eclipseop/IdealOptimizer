e1d7bee13ab2c04436df2127973d90c5c8a26546
eclipseop <guest173800@gmail.com>
2021-04-05 14:42:21 -0400

    start of pictures :)

--- app/pages/comps/Data.tsx
+++ app/pages/comps/Data.tsx
@@ -50,13 +50,59 @@ const DisplayData: React.FC<Props> = (props: Props) => {
 	const test = (floor: number) => {
 		const ph = possibleHazards(floor);
 		if (ph.length == 0) {
-			return <div></div>;
+			return;
 		}
 		return (
-			<div className="break-all">
-				{floor} - {JSON.stringify(ph)}
+			<div className="break-all" key={floor}>
+				{floor}-
+				<div className="grid grid-cols-10">
+					{ph.map((e) => {
+						return getImage(e);
+					})}
+				</div>
 			</div>
 		);
+	};
+
+	const getImage = (id) => {
+		let tag = "/imgs/";
+		switch (id) {
+			case "Resolve":
+				tag += "resolve.png";
+				break;
+			case "PoisonSpawn":
+				tag += "poison.png";
+				break;
+			case "ComboShield":
+				tag += "combo.png";
+				break;
+			case "Tape":
+				tag += "tape.png";
+				break;
+			case "Blind":
+			case "SuperBlind":
+				tag += "blind.png";
+				break;
+			case "SkillBind":
+				tag += "sbr.png";
+				break;
+			case "VDP":
+				tag += "vdp.png";
+				break;
+			case "Cloud":
+				tag += "cloud.png";
+				break;
+			case "AwokenBind":
+				tag += "abind.png";
+				break;
+			case "Skyfall_Jammer":
+				tag += "jammer_surge.png";
+				break;
+			default:
+				tag += id;
+		}
+
+		return <img src={tag} width="30" height="30"></img>;
 	};
 
 	return (
